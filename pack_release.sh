e70ccb65112d437f5675e2ec7f13a4dd703583f9
Aabish Malik <aabishmalik.dev@proton.me>
2024-05-21 23:06:11 +0000

    update pack script

--- pack_release.sh
+++ pack_release.sh
@@ -22,27 +22,35 @@ ref=$(git rev-parse --short HEAD)
 echo $ref > release/commit_hash
 version=$(git describe --tags)
 
+pkging_version="v$version-$ref"
+
 
 # create tarball
-tar -czvf graphs-$version-$ref.tar.gz release/*
+tar -czvf graphs-$pkging_version.tar.gz release/*
 
 # remove release directory
 rm -rf release
 
 echo "Packaging complete"
-echo "Tarball: graphs-$version-$ref.tar.gz"
+echo "Tarball: graphs-$pkging_version.tar.gz"
 
 echo "Signing tarball"
-gpg --detach-sign graphs-$version-$ref.tar.gz
+gpg --detach-sign graphs-$pkging_version.tar.gz
 echo "Tarball signed"
 
 echo "Creating checksum"
-sha256sum graphs-$version-$ref.tar.gz > graphs-$version-$ref.tar.gz.sha256
+sha256sum graphs-$pkging_version.tar.gz > graphs-$pkging_version.tar.gz.sha256
 echo "Checksum created"
 
 # assume commands run using github actions
 echo "Creating Github release"
-gh release create $version-$ref graphs-$version-$ref.tar.gz graphs-$version-$ref.tar.gz.sig graphs-$version-$ref.tar.gz.sha256
+gh release create $pkging_version graphs-$pkging_version.tar.gz graphs-$pkging_version.tar.gz.sig graphs-$pkging_version.tar.gz.sha256
 echo "Github release created"
 
+
+if [ ! -z "GITHUB_ACTION"  ]; then
+	rm -rf $script_dir/build/graphs-$pkging_version.tar.gz
+	rm -rf $script_dir/build/graphs-$pkging_version.tar.gz.sig
+	rm -rf $script_dir/build/graphs-$pkging_version.tar.gz.sha256
+fi
 
